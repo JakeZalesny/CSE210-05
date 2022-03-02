92c4e5f2797d5fa6bec0b78eaceba2ca25fcf745
Jake Zalesny <jrzalesny@gmail.com>
2022-03-02 08:39:51 -0800

    Sneakity Snake

---
+++ snake-incomplete/snake/game/scripting/move_actors_action.py
@@ -0,0 +1,29 @@
+from scripting.action import Action
+import pyray
+
+class MoveActorsAction(Action) :
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def execute(self, cast, script):
+
+        actors = cast.get_all_actors()
+        for i in actors:
+            i.move_next()
+
+        # score = cast.get_first_actor("scores")
+        # food = cast.get_first_actor("foods")
+        # snake = cast.get_first_actor("snakes")
+        # segments = snake.get_segments()
+        # messages = cast.get_actors("messages")
+
+        
+
+    
+    
+
+
+# Override the execute(cast, script) method as follows:
+# 1) get all the actors from the cast
+# 2) loop through the actors
+# 3) call the move_next() method on each actor
