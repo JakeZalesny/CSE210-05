fee2c65f7008bf3825c2eb7e71cbc430355064a7
Jake Zalesny <jrzalesny@gmail.com>
2022-03-09 08:45:47 -0800

    updated anti-collisions

--- snake-incomplete/snake/game/scripting/control_actors_action_RED.py
+++ snake-incomplete/snake/game/scripting/control_actors_action_RED.py
@@ -31,19 +31,19 @@ class ControlActorsActionRed(Action):
             script (Script): The script of Actions in the game.
         """
         # left
-        if self._keyboard_service.is_key_down('j') and self._direction != Point(constants.CELL_SIZE, 0) :
+        if self._keyboard_service.is_key_down('j') and self._direction != constants.RIGHT :
             self._direction = Point(-constants.CELL_SIZE, 0)
         
         # right
-        if self._keyboard_service.is_key_down('l') and self._direction != Point(-constants.CELL_SIZE, 0)  :
+        if self._keyboard_service.is_key_down('l') and self._direction != constants.LEFT  :
             self._direction = Point(constants.CELL_SIZE, 0)
         
         # up
-        if self._keyboard_service.is_key_down('i') and self._direction != Point(0, constants.CELL_SIZE):
+        if self._keyboard_service.is_key_down('i') and self._direction != constants.DOWN:
             self._direction = Point(0, -constants.CELL_SIZE)
         
         # down
-        if self._keyboard_service.is_key_down('k') and self._direction != Point(0, -constants.CELL_SIZE):
+        if self._keyboard_service.is_key_down('k') and self._direction != constants.UP:
             self._direction = Point(0, constants.CELL_SIZE)
         
         red_snake = cast.get_first_actor("snake_red")
